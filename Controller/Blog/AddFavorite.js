8f7b88063fbf7c2f08c4ba0c4c90e96defbd4437
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-12 14:33:52 +0600

    add favorite api : if user already exist then remove user from liked array

--- Controller/Blog/AddFavorite.js
+++ Controller/Blog/AddFavorite.js
@@ -7,7 +7,33 @@ const addFavorite = async (req, res) => {
     console.log("hit favorite ");
 
     const { userEmail } = req.body;
+    const id = req.params.id;
 
+    const blog = await BlogModel.findById({ _id: id });
+
+    const emailIndex = blog.liked.indexOf(userEmail);
+
+    console.log("email index = ", emailIndex);
+
+    if (emailIndex !== -1) {
+      // function for decrease like count
+      const updatedData = await BlogModel.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $dec: {
+            favCount: 1,
+          },
+        }
+      );
+
+      updatedData.save();
+
+      blog.liked.splice(emailIndex, 1);
+      await blog.save();
+      return res.send({ liked: 0 });
+    }
+
+    // function for increase like count
     const updatedData = await BlogModel.findOneAndUpdate(
       { _id: req.params.id },
       {
@@ -20,7 +46,7 @@ const addFavorite = async (req, res) => {
     updatedData.liked.push(userEmail);
     const result = await updatedData.save();
 
-    console.log("result = ", result);
+    // console.log("result = ", result);
 
     res.send({ message: "data updated " });
   } catch (error) {
