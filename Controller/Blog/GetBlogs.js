94be7475a4cb3be211c8a332f446d816b9056da4
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-01-21 22:56:24 +0600

    get blogs : limit , skip num error solved

--- Controller/Blog/GetBlogs.js
+++ Controller/Blog/GetBlogs.js
@@ -6,12 +6,15 @@ const BlogModel = new mongoose.model("Blog", BlogSchema);
 
 const getBlog = async (req, res) => {
   try {
+    const { limit, skip } = req.query;
     const perPageItem = parseFloat(limit);
     const skipNum = parseFloat(skip);
 
     const skipItem = skipNum * perPageItem;
 
     const result = await BlogModel.find().limit(perPageItem).skip(skipItem);
+
+    console.log(result);
 
     res.send({ result });
   } catch (error) {
